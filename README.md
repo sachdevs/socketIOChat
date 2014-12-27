7d837a4386f3589bbd981291a2fa5f12fa8da5c3
sachdevs <sachdev.96@gmail.com>
2014-12-27 18:08:07 -0500

    username functionality barely working

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 SocketIO Chat Application
 =========================
-From SocketIO’s documentation.
+From SocketIO’s documentation.<br>
+Currently finished implementation of all steps.<br>
+Next Improvements:<br>
+Broadcast a message to connected users when someone connects or disconnects<br>
+Add support for nicknames<br>
+Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.<br>
+Add “{user} is typing” functionality<br>
+Show who’s online<br>
+Add private messaging
+
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);//new instance of socket using http server object
+var users = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');//serves html file
 });
 
 io.on('connection', function(socket){
+	socket.on('new user', function(data, callback){
+		if(users.indexOf(data) != -1)
+			callback(false);
+		else{
+			callback(true);
+			socket.username = data;
+			users.push(socket.username);
+			io.emit('usernames', users);
+		}
+	});
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
