9e6f9e90ecd7bcc3d6778cd8409960c3efcaba2f
sachdevs <sachdev.96@gmail.com>
2015-09-03 01:26:50 -0400

    scrolling and margin and number bug

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(process.env.PORT || 3000, function(){
-    console.log('listening on *: 30303000');
+    console.log('listening on: 3000');
 });
